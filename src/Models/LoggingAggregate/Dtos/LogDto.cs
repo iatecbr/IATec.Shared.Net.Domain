e82a3a20987e440869bbd11995edc601f3d258a1
Elvys Marcos Ribeiro Bispo <elvys.marcos@gmail.com>
2024-09-23 16:50:44 -0300

    refactor: Refactor logDto

--- src/Models/LoggingAggregate/Dtos/LogDto.cs
+++ src/Models/LoggingAggregate/Dtos/LogDto.cs
@@ -2,23 +2,33 @@ namespace IATec.Shared.Domain.Models.LoggingAggregate.Dtos;
 
 public record LogDto
 {
-    public string Id { get; set; }
-    public string ContainerKey { get; set; }
-    public string Source { get; set; }
-    public string Owner { get; set; }
-    public string Action { get; set; }
-    public string UserId { get; set; }
-    public DateTime Date { get; set; }
-    public string Content { get; set; }
-
+    public string Id { get; init; }
+    public string ContainerKey { get; init; }
+    public string Source { get; init; }
+    public string Owner { get; init; }
+    public string Action { get; init; }
+    public string UserId { get; init; }
+    public DateTime Date { get; init; }
+    public string Content { get; init; }
+    
     public LogDto()
     {
     }
-
-    public LogDto(string id, string containerKey, string source, string owner, string action, string userId,
-        DateTime date, string content)
+    
+    public LogDto(string id, string containerKey, string source, string owner, string action, string userId, DateTime date, string content)
     {
         Id = id;
+        ContainerKey = containerKey;
+        Source = source;
+        Owner = owner;
+        Action = action;
+        UserId = userId;
+        Date = date;
+        Content = content;
+    }
+    
+    public LogDto(string containerKey, string source, string owner, string action, string userId, DateTime date, string content)
+    {
         ContainerKey = containerKey;
         Source = source;
         Owner = owner;
