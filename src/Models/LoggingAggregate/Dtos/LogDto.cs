1d759757fc9f647d2c7563a3c5451eb31aa60a31
Elvys Marcos Ribeiro Bispo <elvys.marcos@gmail.com>
2024-09-23 16:23:28 -0300

    feat: Improves logs dto

--- src/Models/LoggingAggregate/Dtos/LogDto.cs
+++ src/Models/LoggingAggregate/Dtos/LogDto.cs
@@ -1,10 +1,13 @@
 namespace IATec.Shared.Domain.Models.LoggingAggregate.Dtos;
 
-public sealed record LogDto(
-    string ContainerName,
-    string Source,
-    string Owner,
-    string Action,
-    string UserId,
-    object? Content = null
-);
+public record LogDto
+{
+    public string Id { get; init; }
+    public string ContainerKey { get; init; }
+    public string Source { get; init; }
+    public string Owner { get; init; }
+    public string Action { get; init; }
+    public string UserId { get; init; }
+    public DateTime Date { get; init; }
+    public string Content { get; init; }
+}
