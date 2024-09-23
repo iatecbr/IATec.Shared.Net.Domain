0605087340503f5371a295c296e6b080d6008c0e
Elvys Marcos Ribeiro Bispo <elvys.marcos@gmail.com>
2024-09-23 16:54:56 -0300

    refactor: Refactor logDto

--- src/Models/LoggingAggregate/Dtos/LogDto.cs
+++ src/Models/LoggingAggregate/Dtos/LogDto.cs
@@ -10,31 +10,4 @@ public record LogDto
     public string UserId { get; init; }
     public DateTime Date { get; init; }
     public string Content { get; init; }
-    
-    public LogDto()
-    {
-    }
-    
-    public LogDto(string id, string containerKey, string source, string owner, string action, string userId, DateTime date, string content)
-    {
-        Id = id;
-        ContainerKey = containerKey;
-        Source = source;
-        Owner = owner;
-        Action = action;
-        UserId = userId;
-        Date = date;
-        Content = content;
-    }
-    
-    public LogDto(string containerKey, string source, string owner, string action, string userId, DateTime date, string content)
-    {
-        ContainerKey = containerKey;
-        Source = source;
-        Owner = owner;
-        Action = action;
-        UserId = userId;
-        Date = date;
-        Content = content;
-    }
 }
