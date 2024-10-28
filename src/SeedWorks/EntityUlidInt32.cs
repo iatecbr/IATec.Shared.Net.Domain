ab446df00972978df013f062969c272e2708c74c
jonathan.jimenez <jonathan.jimenez@iatec.com>
2024-10-28 17:35:39 -0300

    feat: Updated name from SecondId to ExternalId

--- src/SeedWorks/EntityUlidInt32.cs
+++ src/SeedWorks/EntityUlidInt32.cs
@@ -2,7 +2,7 @@ namespace IATec.Shared.Domain.SeedWorks;
 
 public class EntityUlidInt32 : Entity<int>
 {
-    public string SecondId { get; private set; } = Ulid.NewUlid().ToString();
+    public string ExternalId { get; private set; } = Ulid.NewUlid().ToString();
     
     public override string GetOwner()
     {
